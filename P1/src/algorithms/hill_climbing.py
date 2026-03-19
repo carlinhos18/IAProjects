8b22978323b81507ba24c189ad0cf9d3fff65d0e
Tiago <up202304762@fe.up.pt>
2026-03-19 21:12:35 +0000

    Now HC returns the dic

--- P1/src/algorithms/hill_climbing.py
+++ P1/src/algorithms/hill_climbing.py
@@ -1,13 +1,11 @@
 import os
-import sys #patch para correr 
+import sys #patch para correr ,depois tirar
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import file_parser
 import random
 from classes import Library,Book
-#Podiamos implementar Stochastic Hill climing e First-Choice hill climbing e random.restart
 
-
-all_books, all_libraries, deadline= file_parser.parse_file("b_read_on.in")
+all_books, all_libraries, deadline= file_parser.parse_file("a_example.in")
 
 
 def cal_score(l1:list)->int:
@@ -15,7 +13,7 @@ def cal_score(l1:list)->int:
     t = 0
     score = 0
     i = 0
-
+    dictionary={}
     while i < len(l1):
         current_lib = l1[i]
         t += current_lib.sign_up_time
@@ -23,15 +21,16 @@ def cal_score(l1:list)->int:
         if t >= deadline:
             break
 
-        days_available = deadline - t
-        total_scannable = days_available * current_lib.shipping_cap
+        rem_days = deadline - t
+        nr_books = rem_days * current_lib.shipping_cap
         f_books = [b for b in current_lib.books if b not in scanned_books]
-        newly_scanned = f_books[:total_scannable] #A logica é temos x dias disponiveis para ler x*capaicdade de livraria ,e os livros ja estao por ordem de valor logo ficamos com os melhores livros
-        scanned_books.update(newly_scanned)
-        score += sum(b.score for b in newly_scanned)
+        new_scanned = f_books[:nr_books] #A logica é temos x dias disponiveis para ler x*capaicdade de livraria ,e os livros ja estao por ordem de valor logo ficamos com os melhores livros
+        scanned_books.update(new_scanned)
+        dictionary[current_lib]=new_scanned
+        score += sum(b.score for b in new_scanned)
         i += 1
     
-    return score
+    return dictionary,score
 
 #Maneira de chegar a um neighbor
 def swap(lst):
@@ -50,15 +49,13 @@ def genFirstRandom()->list:#Podiamos meter greedy choice
     t=0
     shuffled=all_libraries.copy()
     random.shuffle(shuffled)
-    """     for i in shuffled:
-        i.print() """
     for lib in shuffled:
         if t>=deadline:
             break
         if t+lib.sign_up_time<=deadline:
             ans.append(lib)
             t+=lib.sign_up_time
-    #print(f"First sOL{ans}")
+
     return ans
 
 
@@ -67,15 +64,16 @@ def genFirstRandom()->list:#Podiamos meter greedy choice
 
 def hill_climbing_stochastic_choice(prob:float):
     cur=genFirstRandom()
-    score=cal_score(cur)
+    solution,score=cal_score(cur)
     i=0
     tries=0
     while tries<6: #pode dar loop
         ngh=swap(cur)
-        ngh_cr=cal_score(ngh)
+        dictionaryb,ngh_cr=cal_score(ngh)
         if ngh_cr>score:
             cur=ngh
             score=ngh_cr
+            solution=dictionaryb
             tries=0
         elif random.random()<prob:
             cur=ngh
@@ -84,35 +82,41 @@ def hill_climbing_stochastic_choice(prob:float):
         else:
             tries+=1
         i+=1
-    #print(f"Final score : {score}")
-    return cur,score
+
+    return solution,score
+
 def hill_climbing_first_choice():
     cur=genFirstRandom()
-    score=cal_score(cur)
+    solution,score=cal_score(cur)
     i=0
     tries=0
     while tries<6: #pode dar loop
         ngh=swap(cur)
-        ngh_cr=cal_score(ngh)
+        dic,ngh_cr=cal_score(ngh)
         if ngh_cr>score:
             cur=ngh
             score=ngh_cr
             tries=0
+            solution=dic
         else:
             tries+=1
         i+=1
-   # print(f"Final score : {score}")
-    return cur,score
+
+    return solution,score
+
+
 def hill_climbing_random_restart(n:int):
     ans=0
+    solution=dict()
     for i in range(n):
-        _,tmp=hill_climbing_first_choice()
-        ans=max(tmp,ans)
-       # print(f"TMP score{tmp}")
-    return ans
-a=hill_climbing_random_restart(100)
-_,b=hill_climbing_first_choice()
-_,c=hill_climbing_stochastic_choice(0.1)
-print(a)
-print(b)
-print(c)
+        dic,tmp=hill_climbing_first_choice()
+        if ans<tmp:
+            solution=dic
+            ans=tmp
+
+    return solution,ans
+
+
+       
+
+
