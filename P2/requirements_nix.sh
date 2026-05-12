8719fc1be865dc15c394876a92dc05bc6f92cc96
PatinhoF <guitriaes@gmail.com>
2026-05-12 10:20:13 +0100

    nixbtw

--- P2/requirements_nix.sh
+++ P2/requirements_nix.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-nix-shell -p python313Packages.scikit-learn python313Packages.numpy python313Packages.pandas
+nix-shell -p python313Packages.scikit-learn \
+    python313Packages.numpy \
+    python313Packages.pandas \
+    python313Packages.livereload
